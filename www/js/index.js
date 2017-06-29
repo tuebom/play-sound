ad16722e29997f67bff3ecf39085afafebcb3bc1
tuebom <tuebom.wirya@gmail.com>
2017-06-29 15:11:44 +0800

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -2,13 +2,13 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
 	document.querySelector("#playMp3").addEventListener("touchend", playMP3, false);
-  navigator.notification.vibrate( 1000 );
 };
 
 function playMP3() {
     var mp3URL = getMediaURL("sounds/message.mp3");
     var media = new Media(mp3URL, null, mediaError);
     media.play();
+    navigator.notification.vibrate( 1000 );
 }
 
 function getMediaURL(s) {
